090ceac800eef345f0a85266e74a372afe22d1e2
fleety <nmlnk.d@gmail.com>
2023-06-19 15:25:19 +0600

    edit footer

--- src/components/Landing/Footer.jsx
+++ src/components/Landing/Footer.jsx
@@ -1,31 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-              <div class="col-md-4 d-flex align-items-center">
-                <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Mediahate</span>
-              </div>
-              <ul class="nav navbar-nav d-flex flex-row gap-2">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Последние релизы</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Контакты</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Артисты</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Лейбл</a>
-                  </li>
-              </ul>
-            </footer>
+      <div class="container">
+        <div className="content">
+          <div className="footer-description">
+            <span>© 2023 Mediahate</span>
           </div>
+          <ul className="footer-links">
+            <li><a href="#">Последние релизы</a></li>
+            <li><a href="#">Контакты</a></li>
+            <li><a href="#">Артисты</a></li>
+            <li><a href="#">Лейбл</a></li>
+          </ul>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
